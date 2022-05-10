a9bf9d1e1ae19bf6e275a4b0ea64b48442f05189
Maciej Chotkowski <ma.chotkowski@gmail.com>
2022-05-10 21:29:51 +0200

    Cleanup

---
+++ files/hanoi.fish
@@ -0,0 +1,5 @@
+        010&{"ABC"6[  }}}:1=?v  :1-{:}${{:{:@}}$}}}}6a*6+0{{{{f2+0.b3*2.
+ ;voooooon&:+1&". Move from "<  }}}:1-{:}${{:{:@}}$}}}}7a*0+1{{{{@f2+0.{{~~~~].
+  >oooooo{o{{" ot "ooooo~~]a"."oo." morf evoM ."&1+:&noooooooooooo:o" ot "oooo{{:oa"."ooa3*1.
+
+  
